5a5148d5e6f629a629f8717822e1989a80cc0cc8
jchenriquez <jc.henriquez04@gmail.com>
2019-12-07 15:43:32 -0500

    Submitting final solution.

--- main.go
+++ main.go
@@ -10,29 +10,34 @@ func Less (word1, word2 string, alphaDict map[byte] int) bool {
 	minLength := int(math.Min(float64(len(word1)), float64(len(word2))))
 	i := 0
 	word1CurrentOrder := 0
-  word2CurrentOrder := 0
+  	word2CurrentOrder := 0
 
   for ; i < minLength; i++ {
     char1Value,_ := alphaDict[word1[i]]
     char2Value,_ := alphaDict[word2[i]]
 
-    word1CurrentOrder+=char1Value
-    word2CurrentOrder+=char2Value
 
-    if word1CurrentOrder > word2CurrentOrder {
-      return false
+    if char1Value < char2Value && word1CurrentOrder == word2CurrentOrder {
+      return true
     }
+
+    if char1Value > char2Value && word1CurrentOrder == word2CurrentOrder {
+    	return false
+	}
+
+  	word1CurrentOrder+=char1Value
+  	word2CurrentOrder+=char2Value
   }
 
-  if word1CurrentOrder == word2CurrentOrder && len(word1) > i {
-    return false
-  }
+	if word1CurrentOrder == word2CurrentOrder && len(word1) > i {
+    	return false
+  	}
 
   return true
 }
 
 func isAlienSorted(words []string, order string) bool {
-	alphaDict := make(map[byte] int)
+	alphaDict := make(map[byte] int, len(order))
 	for i := 0; i < len(order); i++ {
 		alphaDict[order[i]] = i
 	}
